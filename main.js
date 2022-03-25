d95530d84884923628fc929c6cc5405569eb2dff
Lautaro Vilas <vilaslautaro+git@gmail.com>
2022-03-25 20:16:49 -0300

    bug element 'noMore' fixed. Style 100% responsive

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 'use strict'
 
+const container = document.getElementById('container')
 const peliculas = document.getElementById('peliculas')
 const documentFragment = document.createDocumentFragment()
 const avengers = document.getElementById('avengers')
 const dc = document.getElementById('dc')
 const taquilleras = document.getElementById('taquilleras')
 let contador = 0;
-
-
-avengers.addEventListener('click', () => cargarPeliculas(4, 1))
-dc.addEventListener('click', () => cargarPeliculas(4, 3))
-taquilleras.addEventListener('click', () => cargarPeliculas(4, 10))
 
 const crearEstructuraPelicula = (titulo, img, descripcion) => {
 
@@ -36,8 +32,8 @@ const cargarMasPeliculas = (entry) => {
 
 const observer = new IntersectionObserver(cargarMasPeliculas)
 
-const cargarPeliculas = async (num, id) => {
-    const api = fetch(`https://api.themoviedb.org/4/list/${id}?api_key=c978e7aae993c105bd08d83ddf603d6e&language=es`, {
+const cargarPeliculas = async (num) => {
+    const api = fetch(`https://api.themoviedb.org/4/list/1?api_key=c978e7aae993c105bd08d83ddf603d6e&language=es`, {
         headers: {
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTc4ZTdhYWU5OTNjMTA1YmQwOGQ4M2RkZjYwM2Q2ZSIsInN1YiI6IjYxZmM1YTFiNjg5MjljMDBhNDg1ZTFiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wsswZMRzrZf9YekBi7H32dwGrjfAnCFPD3P2xxmfODY',
             'Content-Type': 'application/json;charset=utf-8'
@@ -46,7 +42,6 @@ const cargarPeliculas = async (num, id) => {
     let request = await api
     let data = await request.json()
     const arr = data.results
-    console.log(data)
     for (let i = 0; i < num; i++) {
         if (arr[contador] !== undefined) {
             const url = arr[contador].poster_path
@@ -56,19 +51,19 @@ const cargarPeliculas = async (num, id) => {
             const pelicula = crearEstructuraPelicula(titulo, img, descripcion)
             documentFragment.appendChild(pelicula)
             contador++
-            if(i == num - 2) observer.observe(pelicula)
-        } else{
-            if(peliculas.lastElementChild.id !== "noMore"){
+            if (i == num - 2) observer.observe(pelicula)
+        } else {
+            if (container.lastElementChild.id !== "noMore") {
                 let noMore = document.createElement('h4')
                 noMore.textContent = "No hay mas peliculas para mostrar"
                 noMore.id = "noMore"
                 documentFragment.appendChild(noMore)
-                peliculas.appendChild(documentFragment)
+                container.appendChild(documentFragment)
                 break;
             }
-        }
     }
-    peliculas.appendChild(documentFragment)
+}
+peliculas.appendChild(documentFragment)
 }
 
-cargarPeliculas(4, 1)
+cargarPeliculas(4)
